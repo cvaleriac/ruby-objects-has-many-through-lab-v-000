5eb1c05c95aa95d53ef05de3f3c75b07de866137
valeria campilongo <cvaleriac@hotmail.it>
2018-06-15 15:58:39 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,4 +11,15 @@ class Doctor
     @@all
    end
 
+   def new_appointment(patient,date)
+     Appointment.new(patient, self, date)
+   end
+   def appointments
+     Appointment.all.select { |appointment| appointment.doctor == self}
+   end
+
+   def patients
+     appointments.map{|appointment| appointment.patient}
+   end
+
  end
